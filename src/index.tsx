7f73c7465028b52782c5e9e5c84a7ddb31869da8
Madhurya Sudhindra <madhurya.sudhindra@exathought.com>
2024-08-30 12:52:42 +0530

    sw is now service worker

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ serviceWorkerRegistration.register(); */
 // src/index.js
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/service-worker.js')
       .then((registration) => {
         console.log('Service Worker registered with scope:', registration.scope);
       })
