9fee4a4a23bec7056c3d3e79339de6752903fdad
Madhurya Sudhindra <madhurya.sudhindra@exathought.com>
2024-09-03 12:50:45 +0530

    mouse origin

--- src/components/pages/Office/VRSetUp.tsx
+++ src/components/pages/Office/VRSetUp.tsx
@@ -1,36 +1,74 @@
-import React from 'react';
-import 'aframe';
+import React, { useEffect, useState } from 'react';
 import { Entity } from 'aframe-react';
 
-const VRSetup: React.FC = () => (
-  <>
-    <Entity laser-controls="hand: left" line="color: red; opacity: 0.5;" raycaster="objects: .clickable" position="-0.5 1.5 0" />
-    <Entity laser-controls="hand: right" line="color: blue; opacity: 0.5;" raycaster="objects: .clickable" position="0.5 1.5 0" />
+const VRSetup = () => {
+  const [isWebXRSupported, setIsWebXRSupported] = useState(false);
+
+  // Function to check WebXR support
 
 
-{/*     <Entity
-    hand-tracking-controls="hand: left"
-    laser-controls="hand: left"
-    oculus-touch-controls="hand: left"
-    raycaster="objects: .clickable"
-    line="color: red; opacity: 0.5"
-    
-  />
+  const checkWebXRSupport = () => {
+    if ('xr' in navigator) {
+      const xr = (navigator as any).xr;
+      xr.isSessionSupported('immersive-vr').then((supported: boolean) => {
+        if (supported) {
+          setIsWebXRSupported(supported);
+          console.log('WebXR  supported,', supported);
+        } else {
+          setIsWebXRSupported(false);
+          console.log('WebXR not supported');
+        }
+      });
+    }
+  };
 
-  {/* Right hand /}
-  <Entity
-    hand-tracking-controls="hand: right"
-    laser-controls="hand: right"
-    oculus-touch-controls="hand: right"
-    raycaster="objects: .clickable"
-    line="color: blue; opacity: 0.5"
-  /> */}
 
-  
-    <Entity primitive="a-camera" position='0 4.6 0' rotation='0 50 0'>
-      <Entity primitive="a-cursor"  position="1 1 1 " raycaster="objects: .clickable" />
-    </Entity>
-  </>
-);
+
+  // Check WebXR support on component mount
+  useEffect(() => {
+    checkWebXRSupport();
+  }, []);
+
+  return (
+    <>
+      {/* Left and Right Controllers */}
+      <Entity
+        laser-controls="hand: left"
+        line="color: red; opacity: 0.5;"
+        raycaster="objects: .clickable"
+        position="-0.5 1.5 0"
+      />
+      <Entity
+        laser-controls="hand: right"
+        line="color: blue; opacity: 0.5;"
+        raycaster="objects: .clickable"
+        position="0.5 1.5 0"
+      />
+
+
+
+      {isWebXRSupported ? (
+  <Entity primitive="a-camera" position="0 4.6 0" rotation="0 50 0">
+    <Entity
+      primitive="a-cursor"
+      position="1 1 1"
+      raycaster="objects: .clickable"
+    />
+  </Entity>
+) : (
+  <Entity primitive="a-camera" position="0 4.6 0" rotation="0 50 0">
+    <Entity
+      primitive="a-cursor"
+      raycaster="objects: .clickable"
+      position="0 0 -1"
+      geometry="primitive: ring; radiusInner: 0.01; radiusOuter: 0.02"
+      material="color: white; shader: flat"
+      cursor="fuse: false; rayOrigin: mouse"
+    />
+  </Entity>
+)}
+    </>
+  );
+};
 
 export default VRSetup;
